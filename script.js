4ea38d0f1cdf79c16888d69eabc446cc7c37df4d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-28 19:50:27 -0500

    Populte voices in to the dropdown

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  const voiceOptions = voices
+    .map(voice => `<option value="${voice.name}">${voice. name} (${voice.lang})</option>`)
+    .join('');
+  voicesDropdown.innerHTML = voiceOptions;
+}
+
+// Populate voices
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
