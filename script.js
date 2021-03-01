9f9ffcf8756169fa0febde5d32d6c6c9f8efda4e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-28 20:17:05 -0500

    Add functionality to buttons

--- script.js
+++ script.js
@@ -35,3 +35,5 @@ function setOption() {
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
 options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', toggle.bind(null, false));
